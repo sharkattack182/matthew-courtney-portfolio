ef815302a624b0c9c46b2240eefb8d23b6716ec9
Matthew Courtney <matthewdc182@gmail.com>
2020-11-11 19:52:57 -0500

    trouble getting the components to render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Sidebar from './components/Sidebar/sidebar'
+import Welcome from './components/Welcome/welcome'
 
 function App() {
   return (
     <div className="App">
-  
+        
+        <Sidebar />
+        {/* <Router>
+        
+        <Route exact path="/" component={Welcome} />
+        </Router> */}
     </div>
   );
 }
---
+++ src/components/Welcome/welcome.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './welcome.css';
+
+function Body() {
+    return (
+        <div className="site-main">
+            <div className="site-main-inner">
+                <div className="home-section">
+                    <div className="home-section-inner">
+                        <div className="home-heading">
+                            Hi, I'm Matt,
+                </div>
+
+                        <h2 className="home-subheading">
+                            a freelance software engineer specialized in<br />
+                    both <strong>front-end and back-end </strong>. Ipdfkmsdkfsndlfknsdljgnsdlkfnak;fnapwfnm;akdnfsdngka;dfma;skf'ask'dfaksfpkadfl;mazdlfmas;lfmals;f</h2>
+
+
+                        <div className="home-buttons">
+                            <a href="/portfolio/" className="button button-outline button-white">Portfolio</a>
+                            <a href="/contact/" className="button button-outline button-white">Contact</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Body;       
