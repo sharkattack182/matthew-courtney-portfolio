493d4321397c2b77507d9133ab8c51fc593b3dfe
Matthew Courtney <matthewdc182@gmail.com>
2020-11-16 14:20:37 -0500

    worked on port 1 page resizing need to do the reactive version

--- src/components/Port1/port1.js
+++ src/components/Port1/port1.js
@@ -1,13 +1,28 @@
 import React from "react";
 import './port1.css';
+import Example from "./imgs/equippe.png"
 
 
 function Port1() {
     return (
         <div className="container-port">
             <div className="blue">
-                <h1 className="hello">EquiPPE</h1>
-                <p>EquiPPE served as my final project for my bootcamp. The idea behind the project was to build an ecommerce site. I worked with a team of three other students. I was responsible for architecting the overall project design including managing the team respository and delegating initail responsibilities for each team member. I worked on the backend by creating and integrating the database using sequelize. I als took on the responsibility of defining the server side routes and controllers using Exoress. After an MVP was met I also went through and stylized the final product using HTML, CSS, and JavaScript.</p>
+            <div className="row">
+                <div className="col-md-10">
+                    <h1 className="title">EquiPPE</h1>
+                </div>
+            </div>
+                <div className="row">
+                    <div className="col-md-10">
+                        <p>EquiPPE served as my final project for my bootcamp. The idea behind the project was to build an ecommerce site. I worked with a team of three other students. I was responsible for architecting the overall project design including managing the team respository and delegating initail responsibilities for each team member. I worked on the backend by creating and integrating the database using sequelize. I als took on the responsibility of defining the server side routes and controllers using Exoress. After an MVP was met I also went through and stylized the final product using HTML, CSS, and JavaScript.</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-10">
+                        <img src={Example} alt="equippe" className="image1"/> 
+                    </div>
+                </div>
+
             </div>
         </div>
 
