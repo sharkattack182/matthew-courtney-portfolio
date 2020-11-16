5df2283ea2932c5a7bef2836ad8ddb27d1bac15a
Matthew Courtney <matthewdc182@gmail.com>
2020-11-16 15:52:47 -0500

    added in responsive design for the port1

--- src/components/Port1/port1.js
+++ src/components/Port1/port1.js
@@ -14,7 +14,7 @@ function Port1() {
             </div>
                 <div className="row">
                     <div className="col-md-10">
-                        <p>EquiPPE served as my final project for my bootcamp. The idea behind the project was to build an ecommerce site. I worked with a team of three other students. I was responsible for architecting the overall project design including managing the team respository and delegating initail responsibilities for each team member. I worked on the backend by creating and integrating the database using sequelize. I als took on the responsibility of defining the server side routes and controllers using Exoress. After an MVP was met I also went through and stylized the final product using HTML, CSS, and JavaScript.</p>
+                        <p>EquiPPE served as my final project for my bootcamp. The idea behind the project was to build an ecommerce site. I worked with a team of three other students. I was responsible for architecting the overall project design including managing the team respository and delegating initail responsibilities for each team member. I worked on the backend by creating and integrating the database using sequelize. I also took on the responsibility of defining the server side routes and controllers using Express. After an MVP was met I also went through and stylized the final product using HTML, CSS, and JavaScript.</p>
                     </div>
                 </div>
                 <div className="row">
