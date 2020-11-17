3e018ba65c1b1d87c0af3498aa736638d9a98bfd
Matthew Courtney <matthewdc182@gmail.com>
2020-11-16 23:37:44 -0500

    added in css used the same from the other card need to change content

--- src/components/Port2/port2.js
+++ src/components/Port2/port2.js
@@ -1,13 +1,30 @@
 import React from "react";
 import './port2.css';
+import Example2 from "./imgs/employee.png"
 
 
 function Port2() {
     return (
-        <div className="container-custom">
-<h1 className="hello">Hello World2</h1>
-
+        <div className="container-port">
+        <div className="blue">
+        <div className="row">
+            <div className="col-md-10">
+                <h1 className="title">Employee Tracker</h1>
+            </div>
         </div>
+            <div className="row">
+                <div className="col-md-10">
+                    <p>EquiPPE served as my final project for my bootcamp. The idea behind the project was to build an ecommerce site. I worked with a team of three other students. I was responsible for architecting the overall project design including managing the team respository and delegating initail responsibilities for each team member. I worked on the backend by creating and integrating the database using sequelize. I also took on the responsibility of defining the server side routes and controllers using Express. After an MVP was met I also went through and stylized the final product using HTML, CSS, and JavaScript.</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-10">
+                    <img src={Example2} alt="employee-tracker" className="image2"/> 
+                </div>
+            </div>
+            <a href="https://github.com/sharkattack182/react-employee" target="_blank" className="button button-outline button-white">Github</a>
+        </div>
+    </div>
 
         
 
