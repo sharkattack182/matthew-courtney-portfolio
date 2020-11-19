10451c647dc4771ebb11e4da9c78693962c965bf
Matthew Courtney <matthewdc182@gmail.com>
2020-11-19 14:20:01 -0500

    added in pokedex paragraph

--- src/components/Port3/port3.js
+++ src/components/Port3/port3.js
@@ -5,25 +5,29 @@ import Example3 from "./imgs/pokedex.png"
 function Port3() {
     return (
         <div className="container-port">
-        <div className="blue">
-        <div className="row">
-            <div className="col-md-10">
-                <h1 className="title">Pokedex</h1>
+            <div className="blue">
+                <div className="row">
+                    <div className="col-md-10">
+                        <h1 className="title">Pokedex</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-10">
+                        <p>The Pokedex served as my second project for the bootcamp. This was the very first full stack application I built in the class.
+                        In this project I worked to build the app using the MVC paradaigm. I was responsible for building my own server side API as well.
+                        THis project also served as my first time working with linting tools. The app uses a web based API called PokedexAPI which supplied most of the
+                        images and data points. I built API routes to store user data as well as the data (pokemon) associated to their user id.
+                        Overall this project was alot of fun and very nostalgic. </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-10">
+                        <img src={Example3} alt="pokedex" className="image3" />
+                    </div>
+                </div>
+                <a href="https://github.com/sharkattack182/project-2" target="_blank" className="button button-outline button-white">Github</a>
             </div>
         </div>
-            <div className="row">
-                <div className="col-md-10">
-                    <p>EquiPPE served as my final project for my bootcamp. The idea behind the project was to build an ecommerce site. I worked with a team of three other students. I was responsible for architecting the overall project design including managing the team respository and delegating initail responsibilities for each team member. I worked on the backend by creating and integrating the database using sequelize. I also took on the responsibility of defining the server side routes and controllers using Express. After an MVP was met I also went through and stylized the final product using HTML, CSS, and JavaScript.</p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-10">
-                    <img src={Example3} alt="pokedex" className="image3"/> 
-                </div>
-            </div>
-            <a href="https://github.com/sharkattack182/project-2" target="_blank" className="button button-outline button-white">Github</a>
-        </div>
-    </div>
 
 
 
