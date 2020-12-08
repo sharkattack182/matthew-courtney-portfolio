ce1bb8e9fe6d8501e02c87c9c9b901777f71c28d
Matthew Courtney <matthewdc182@gmail.com>
2020-12-07 22:14:04 -0500

    added in a new landing page

---
+++ src/components/Landing Page/landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function LandingPage() {
+    return (
+
+        <div className= "container-flex">
+            
+        </div>
+    )
+}
+
+export default LandingPage;       
