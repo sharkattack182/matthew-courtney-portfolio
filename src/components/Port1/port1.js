ac360ecaa633645fd2484aecf233ea1807b73260
Matthew Courtney <matthewdc182@gmail.com>
2021-01-12 16:08:59 -0500

    restyled the port1 page

--- src/components/Port1/port1.js
+++ src/components/Port1/port1.js
@@ -6,23 +6,26 @@ import Example from "./imgs/equippe.png"
 function Port1() {
     return (
         <div className="container-port">
-            <div className="blue">
+
             <div className="row">
                 <div className="col-md-10">
                     <h1 className="title">EquiPPE</h1>
                 </div>
             </div>
+            <div className="blue">
                 <div className="row">
                     <div className="col-md-10">
-                        <p>EquiPPE served as my final project for my bootcamp. The idea behind the project was to build an ecommerce site. I worked with a team of three other students. I was responsible for architecting the overall project design including managing the team respository and delegating initail responsibilities for each team member. I worked on the backend by creating and integrating the database using sequelize. I also took on the responsibility of defining the server side routes and controllers using Express. After an MVP was met I also went through and stylized the final product using HTML, CSS, and JavaScript.</p>
+                    <img src={Example} alt="equippe" className="image1" />
+                        <p className= "port-description">EquiPPE served as my final project for my bootcamp. The idea behind the project was to build an ecommerce site. I worked with a team of three other students. I was responsible for architecting the overall project design including managing the team respository and delegating initail responsibilities for each team member. I worked on the backend by creating and integrating the database using sequelize. I also took on the responsibility of defining the server side routes and controllers using Express. After an MVP was met I also went through and stylized the final product using HTML, CSS, and JavaScript.</p>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-10">
-                        <img src={Example} alt="equippe" className="image1"/> 
-                    </div>                    
+                <div className="row row1">
+                    <div className= "link-buttons">
+                    <a href="https://github.com/sharkattack182/project-3" target="_blank" className="button button-outline button-white">Github</a>
+                <a href="https://vast-ravine-19894.herokuapp.com/" target="_blank" className="button button-outline button-white">Deployed</a>
+                    </div>
                 </div>
-                <a href="https://github.com/sharkattack182/project-3" target="_blank" className="button button-outline button-white">Github</a>
+                
             </div>
         </div>
 
